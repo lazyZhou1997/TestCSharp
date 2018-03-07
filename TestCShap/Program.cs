7d37d9830158812c9ed4a5789aab829de86b9e48
zhouMac <LazyZhou1997@gmail.com>
2018-03-07 08:47:41 +0800

    2018-3-7第一次测试

---
+++ TestCShap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestCShap
+{
+    class Program
+    {
+
+        static int SumValues(params int[] vals)
+        {
+            int sum = 0;
+            foreach (var val in vals)
+            {
+                sum += val;
+            }
+
+            return sum;
+        }
+        
+        static void Main(string[] args)
+        {
+            int sum = SumValues(1, 5, 2, 9, 8);
+            Console.WriteLine("Summed Values = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
