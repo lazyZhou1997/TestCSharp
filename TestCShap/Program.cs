cab0bf230e8e778f5fc5192b31f1c70b457fce0f
zhouMac <LazyZhou1997@gmail.com>
2018-03-07 09:35:59 +0800

    输出参数

--- TestCShap/Program.cs
+++ TestCShap/Program.cs
@@ -4,19 +4,29 @@ namespace TestCShap
 {
     class Program
     {
-
-        static void ShowDouble(ref int val)
+        static int MaxValue(int[] intArray, out int maxIndex)
         {
-            val *= 2;
-            Console.WriteLine("Val doubled = {0}",val);
+            int maxVal = intArray[0];
+            maxIndex = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] > maxVal)
+                {
+                    maxVal = intArray[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxVal;
         }
-        
+
         static void Main(string[] args)
         {
-            int number = 5;
-            Console.WriteLine("myNumber = {0}",number);
-            ShowDouble(ref number);
-            Console.WriteLine("myNumber = {0}",number);
+            int[] myArray = {1, 8, 3, 6, 2, 5, 9, 3, 0, 2};
+            int maxIndex;
+            Console.WriteLine("The maximumvalue inyArray is {0}", MaxValue(myArray, out maxIndex));
+            Console.WriteLine("{0}", maxIndex + 1);
         }
     }
 }
